52d055d50bc637c12f6254bc3c8a2ec161e49e6f
manuel-0310 <manucastil0310@gmail.com>
2025-04-05 15:33:26 -0500

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,11 +1,19 @@
 document.getElementById('form-contacto').addEventListener('submit', function(e) {
   e.preventDefault();
 
+  const boton = this.querySelector("button");
+  boton.disabled = true;
+  boton.textContent = "Enviando...";
+
   emailjs.sendForm('service_6m7prwn', 'template_wkbb3r9', this)
     .then(function() {
       alert('✅ Mensaje enviado correctamente');
       document.getElementById('form-contacto').reset();
+      boton.disabled = false;
+      boton.textContent = "Enviar";
     }, function(error) {
       alert('❌ Ocurrió un error: ' + JSON.stringify(error));
+      boton.disabled = false;
+      boton.textContent = "Enviar";
     });
 });
